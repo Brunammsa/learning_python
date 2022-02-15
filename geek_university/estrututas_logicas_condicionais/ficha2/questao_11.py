90c397e5d853477bf7a8f9a15c2a2dbf02df3fc5
Brunammsa <98619037+Brunammsa@users.noreply.github.com>
2022-02-15 18:39:23 -0300

    adicionei exclamação na validação

--- geek_university/estrututas_logicas_condicionais/ficha2/questao_11.py
+++ geek_university/estrututas_logicas_condicionais/ficha2/questao_11.py
@@ -2,7 +2,7 @@ numero_digitado = input("Digite um número inteiro maior do que zero: ")
 acumulador = 0
 
 if int(numero_digitado) < 0:
-    print("Número inválido")
+    print("Número inválido!")
 else:
     for digito in numero_digitado:
         acumulador += int(digito)
