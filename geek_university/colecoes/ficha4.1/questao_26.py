646e357c7ab9dd21e88d613e3477a9a8b4dd98e7
Brunammsa <98619037+Brunammsa@users.noreply.github.com>
2022-04-06 17:53:12 -0300

    validação das questões 26 e 27

--- geek_university/colecoes/ficha4.1/questao_26.py
+++ geek_university/colecoes/ficha4.1/questao_26.py
@@ -1,9 +1,14 @@
 import math
 
 vetor_v = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+somatoria = 0
+media_vetor = sum(vetor_v)/len(vetor_v)
 
-media_vetor = (sum(vetor_v)/len(vetor_v)) - 1
-somatoria = sum(media_vetor)
+for i in vetor_v:
+    variancia = (i - media_vetor) ** 2
+    somatoria += variancia
 
-desvio_padrao = math.sqrt()
-# continuar depois
+desvio_padrao = math.sqrt(somatoria * 1/len(vetor_v)-1)
+print(desvio_padrao)
+
+# não sei se ta certo
--- geek_university/colecoes/ficha4.1/questao_27.py
+++ geek_university/colecoes/ficha4.1/questao_27.py
@@ -1,1 +1,16 @@
-vetor = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+vetor = []
+divisores = 0
+
+for i in range(10):
+    numb = int(input("Digite um número para armazena-lo:"))
+    vetor.append(numb)
+    for j in range(1, numb):
+        if numb % j == 0:
+            divisores += 1
+    if divisores > 2:
+        print('{} não é primo'.format(j))
+    else:
+        for index, valor in enumerate(vetor):    
+            print('{} é primo na posição {}'.format(valor, index))
+
+# ainda não to acertandoooooooooooooo
