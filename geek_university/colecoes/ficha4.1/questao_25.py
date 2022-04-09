11ea8e8ec987491cedea8124419e9bcea337fbe7
Brunammsa <98619037+Brunammsa@users.noreply.github.com>
2022-04-09 11:18:53 -0300

    validação das questões 25, 27

--- geek_university/colecoes/ficha4.1/questao_25.py
+++ geek_university/colecoes/ficha4.1/questao_25.py
@@ -2,7 +2,9 @@ vetor = []
 numero = 1
 
 while len(vetor) < 100:
-    if numero % 7 != 0 or str(numero)[len(str(numero))-1] == str(7):
+    numero_str = str(numero)
+    ultima_posicao_numero_str = len(numero_str)-1
+    if numero % 7 != 0 or numero_str[ultima_posicao_numero_str] == str(7):
         vetor.append(numero)
     numero += 1
 print(vetor)
--- geek_university/colecoes/ficha4.1/questao_27.py
+++ geek_university/colecoes/ficha4.1/questao_27.py
@@ -1,10 +1,26 @@
-vetor = [112, 233, 3123, 554, 75, 16, 17, 318, 39, 5510]
+vetor = [112, 233, 3123, 554, 75, 16, 17, 318, 17, 5510]
 divisores = 0
 
-for i in vetor:
-    for j in range(1, i + 1):
-        if i % j == 0:
+"""
+****** Com for ******
+for index, valor in enumerate(vetor):
+    for j in range(1, valor + 1):
+        if valor % j == 0:
             divisores += 1
     if divisores == 2:
-        print('o número {} é primo e sua posição é a {}'.format(j, vetor.index(j)))
+        print('o número {} é primo e sua posição é a {}'.format(j, index))
     divisores = 0
+"""
+# ***** Com while *****
+
+index = 0
+
+while index < len(vetor):
+    valor = vetor[index]
+    for j in range(1, valor + 1):
+        if valor % j == 0:
+            divisores += 1
+    if divisores == 2:
+        print('o número {} é primo e sua posição é a {}'.format(j, index))
+    divisores = 0
+    index += 1
