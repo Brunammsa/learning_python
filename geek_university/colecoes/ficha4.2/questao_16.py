df2ce2715ab96c955a27576c56535c27d3ea80ac
Brunammsa <98619037+Brunammsa@users.noreply.github.com>
2022-04-12 17:28:45 -0300

    validação da questão 16

--- geek_university/colecoes/ficha4.2/questao_16.py
+++ geek_university/colecoes/ficha4.2/questao_16.py
@@ -1,19 +1,73 @@
-gabarito = {
+gabarito_professor = {
     'questão 1': 'b',
     'questão 2': 'c',
     'questão 3': 'a',
-    'questão 4': 'b',
+    'questão 4': 'e',
     'questão 5': 'c',
-    'questão 6': 'd',
+    'questão 6': 'e',
     'questão 7': 'd',
     'questão 8': 'c',
-    'questão 9': 'a',
-    'questão 10': 'b'
+    'questão 9': 'e',
+    'questão 10': 'a',
 }
-print('{}\n'.format(gabarito))
 
-aluno1 = {'bruno': 12345, 'resposta': 'b'}
-aluno2 = {'bruna': 54321, 'resposta': 'd'}
-aluno3 = {'felipe': 67890, 'resposta': 'a'}
-
-# vou continuar depois
+pontuacao_aluno = 0
+sala_de_aula = [
+    {
+    'matricula': 12345,
+    'nota': 0,
+    'gabarito': {
+        'questão 1': 'b',
+        'questão 2': 'd',
+        'questão 3': 'b',
+        'questão 4': 'e',
+        'questão 5': 'd',
+        'questão 6': 'c',
+        'questão 7': 'a',
+        'questão 8': 'a',
+        'questão 9': 'b',
+        'questão 10': 'e'
+        }
+    },{
+    'matricula': 67890,
+    'nota': 0,
+    'gabarito': {
+        'questão 1': 'c',
+        'questão 2': 'c',
+        'questão 3': 'd',
+        'questão 4': 'e',
+        'questão 5': 'b',
+        'questão 6': 'e',
+        'questão 7': 'e',
+        'questão 8': 'c',
+        'questão 9': 'd',
+        'questão 10': 'a'
+        }
+    },{
+    'matricula': 54321,
+    'nota': 0,
+    'gabarito': {
+        'questão 1': 'b',
+        'questão 2': 'c',
+        'questão 3': 'a',
+        'questão 4': 'e',
+        'questão 5': 'c',
+        'questão 6': 'e',
+        'questão 7': 'd',
+        'questão 8': 'c',
+        'questão 9': 'e',
+        'questão 10': 'a'
+        }
+    }
+]
+passou = 0
+for aluno in sala_de_aula:
+    pontuacao_aluno = 0
+    for questao in gabarito_professor.keys():
+        if gabarito_professor[questao] == aluno['gabarito'][questao]:
+            pontuacao_aluno += 1
+    aluno['nota'] = pontuacao_aluno
+    if aluno['nota'] >= 7:
+        passou += 1
+    print('o aluno {} tirou {}, seu gabarito foi: {}\n'.format(aluno['matricula'], aluno['nota'], aluno['gabarito']))
+print('o percentual de aprovação foi {}%'.format(passou / len(sala_de_aula) * 100))
