7c1f6d36786341ceae51137059f669e9dd14acde
Brunammsa <98619037+Brunammsa@users.noreply.github.com>
2022-04-22 17:45:29 -0300

    validação da questão 15

---
+++ geek_university/colecoes/ficha4.2/questao_15.py
@@ -0,0 +1,31 @@
+matriz = [
+    ['a', 'd', 'd', 'c', 'a', 'e', 'e', 'b', 'a', 'd'],
+    ['d', 'c', 'd', 'a', 'a', 'e', 'e', 'd', 'c', 'e'],
+    ['e', 'b', 'd', 'a', 'c', 'c', 'e', 'b', 'd', 'e'], 
+    ['e', 'e', 'a', 'b', 'd', 'c', 'c', 'e', 'd', 'c'], 
+    ['d', 'b', 'd', 'c', 'e', 'a', 'e', 'a', 'c', 'c']
+]
+n = 0
+gabarito = ['a', 'e', 'd', 'c', 'c', 'b', 'a', 'd', 'd', 'a']
+resultado = []
+soma_da_pontuacao = 0
+
+'''
+Extra:
+
+for indice_do_aluno in range(len(matriz)):
+    gabarito_aluno = matriz[indice_do_aluno]
+    print('Aluno {}:'.format(indice_do_aluno))
+    for i in range(len(gabarito_aluno)):
+        print('{}) {}'.format(i, gabarito_aluno[i]))
+'''
+
+for indice_do_aluno, gabarito_aluno in enumerate(matriz):
+    for alternativa in gabarito_aluno:
+        if alternativa == gabarito[n]:
+            soma_da_pontuacao += 1
+        n += 1
+    resultado.append(soma_da_pontuacao)
+    n = 0
+    soma_da_pontuacao = 0
+print(resultado)
