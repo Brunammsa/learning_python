0d84ebf45a94ebcc1609e125e917eda523a41350
Brunammsa <98619037+Brunammsa@users.noreply.github.com>
2022-06-15 16:57:55 -0300

    aula filter adicionada

---
+++ geek_university/expressoes_lambdas/filter.py
@@ -0,0 +1,13 @@
+"""
+Filter
+
+filter() serve para filtrar dados de uma determinada coleção;
+
+"""
+# biblioteca para trabalhar com dados estatísticos
+import statistics
+
+dados = [1.2, 5.5, 9.2, 3.4]
+
+media = statistics.mean(dados)
+
