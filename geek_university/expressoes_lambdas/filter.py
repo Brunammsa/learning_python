83da1db695ddebc6f3b5ce560469b4b3e3311fa5
Brunammsa <98619037+Brunammsa@users.noreply.github.com>
2022-06-16 16:26:39 -0300

    atualização dos assuntos com lambda

--- geek_university/expressoes_lambdas/filter.py
+++ geek_university/expressoes_lambdas/filter.py
@@ -5,9 +5,52 @@ filter() serve para filtrar dados de uma determinada coleção;
 
 """
 # biblioteca para trabalhar com dados estatísticos
-import statistics
+# import statistics (não quer pegar na minha versã)
 
 dados = [1.2, 5.5, 9.2, 3.4]
 
-media = statistics.mean(dados)
+# media = statistics.mean(dados) (precisa do import)
+media = sum(dados)/len(dados)
 
+# Assim como a função map(), o filter() recebe dois parâmetros (uma funçao e um iterável);
+res = filter(lambda x: x > media, dados)
+print(list(res))
+
+# OBS.: Assim como na função map(), após serem utilizados os dados de filter() eles serão excluídos da memória;
+
+paises = ["brasil", "EUA", "", "argentina", "", "", "colombia", ""]
+# Jeito 01
+res = filter(None, paises)
+print(list(res))
+# Jeito 02
+res = filter(lambda pais: len(pais) > 0, paises)
+print(list(res))
+
+
+"""
+Diferenças entre map() e filter ()
+
+map() -> recebe dois parâmetros, uma função e um iterável, e retorna um objeto mapeando a função para cada elemento do iterávl
+
+filter() -> recebe dois parâmetros, uma função e um iterável, e retorna um objeto, filtrando apenas os elementos de acordo com a função
+"""
+
+# Exemplo complexo
+
+usuarios = [
+    {"usurname": "tenorio", "tweets": []},
+    {"usurname": "bruna", "tweets": ["eu amo caranguejo", "amo dormir", "gosto de assistir pantanal"]},
+    {"usurname": "felipe", "tweets": ["adoro diablo immortal", "e assistir anime"]},
+    {"usurname": "bruno", "tweets": []},
+    {"usurname": "vanessa", "tweets": ["bora tomar uma breja"]}
+]
+
+inativos = list(filter(lambda usuario: len(usuario["tweets"]) == 0, usuarios))
+print(inativos)
+
+# Combinando filter() e map()
+
+nomes = ["Vanessa", "Julia", "Renata", "Ana", "tt"]
+
+lista = list(map(lambda nome: "sua instrutora é {}".format(nome), filter(lambda nome: len(nome) < 5, nomes)))
+print(lista)
---
+++ geek_university/expressoes_lambdas/reduce.py
@@ -0,0 +1,43 @@
+"""
+Reduce
+
+OBS.: A partir do Python3+ a função reduce() não é mais uma função integrada (built-in). Agora temos que importar e utilizar esta função a partir do
+módolo 'functools'
+
+Para entender o reduce()
+
+Imagine que você tem uma coleção de dados e uma função que recebe dois parâmetros: reduce(funcao, dados)
+
+dados = [a1, a2, a3...an]
+
+def funcao(x, y):
+    retur x * y
+
+A função reduce funciona da seguinte forma:
+Passo 1:
+res1 = f(a1, a2) Aplica a função noa dois primeiros elementos da coleção e guarda o resultado.
+
+Passo 2:
+res2 = f(res1, a3) Aplica a função passando o resultado do passo 1 mais o terceiro elemento e guarda o resultado e continua assim até o final.
+
+No final de tudo, o reduce() irá retornar o resultado final.
+"""
+
+from functools import reduce
+
+# Na prática com lambda:
+
+dados = [1, 2, 3, 5, 2, 4, 1, 2]
+
+multi = lambda x, y: x * y
+
+res = reduce(multi, dados)
+print(res)
+
+# Utilizando loop normal:
+
+res = 1
+for n in dados:
+    res = res * n
+
+print(res)
